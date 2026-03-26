cd788207f53a72a244e86b3d70236907807c43f7
llll813 <3028368697@qq.com>
2026-03-26 09:33:21 +0800

    计算完成暂未测试

--- JSPT/Chromosome.cpp
+++ JSPT/Chromosome.cpp
@@ -1,4 +1,8 @@
 #include"Chromosome.h"
+#include"yunshutime.h"
+#include"random.h"
+#include"GONGXU.h"
+#include<map>
 Chromosome::Chromosome() 
 {
 	fitness = 0;
@@ -29,9 +33,84 @@ Chromosome& Chromosome::operator=(const Chromosome& other)
 	}
 	return *this;
 }
-void Chromosome::calculate() 
+void Chromosome::schedule_transport(
+	int job_id, int d,
+	const std::vector<GONGJIAN>& jobs,
+	const yunshutime& yt,
+	std::vector<int>& vehicle_free,
+	std::vector<int>& vehicle_pos,
+	std::vector<int>& trans_done)
+{
+	int j = job_id - 1;
+	int n_ops = (int)jobs[j].gongxu_set.size();
+	int v_id = vehicle_assignment[j][d];
+	int from_loc = (d == 0) ? 0 : jobs[j].gongxu_set[d - 1].jiqi_id;
+	int to_loc = (d == n_ops) ? 0 : jobs[j].gongxu_set[d].jiqi_id;
+	int t_vehicle_arrive = vehicle_free[v_id] + yt.kong[vehicle_pos[v_id]][from_loc];
+	int t_ready = (d == 0) ? 0 : op_end[j][d - 1];
+	int t_start = std::max(t_vehicle_arrive, t_ready);
+	int t_delivery = t_start + yt.man[from_loc][to_loc];
+	trans_end[j][d] = t_delivery;
+	vehicle_free[v_id] = t_delivery;
+	vehicle_pos[v_id] = to_loc;
+	trans_done[j]++;
+}
+void Chromosome::calculate(const yunshutime& yt,const std::vector<GONGJIAN>& jobs) 
 {
 	int time = 0;
-
-	fitness = time;
+	int num_jobs = (int)jobs.size();
+	int num_vehicles = 0;
+	for (const auto& va : vehicle_assignment) {
+		for (int v : va) {
+			if (v > num_vehicles) num_vehicles = v;
+		}
+	}
+	std::vector<int>vehicle_free(num_vehicles + 1, 0); // 车辆空闲时间
+	std::vector<int>vehicle_pos(num_vehicles + 1, 0); // 车辆当前位置
+	int num_locs = (int)yt.man.size();
+	std::vector<int>machine_free(num_locs, 0); // 机器空闲时间
+	op_end.resize(num_jobs);
+	trans_end.resize(num_jobs);
+	for (int i = 0; i < num_jobs; ++i)
+	{
+		int n_ops_job = (int)jobs[i].gongxu_set.size();
+		op_end[i].resize(n_ops_job, 0);
+		trans_end[i].resize(n_ops_job + 1, 0);
+	}
+	std::vector<int>op_done(num_jobs, 0); // 记录每个工件当前进行到第几道工序
+	std::vector<int>trans_done(num_jobs, 0); // 记录每个工件当前进行到第几次运输
+	std::map<int, int>op_counter; 
+	for (int id : operation_sequence)
+	{
+		int d = op_counter[id]++;
+		int j = id - 1;
+		if (trans_done[j] < d)
+		{
+			this->schedule_transport(id, d, jobs, yt, vehicle_free, vehicle_pos, trans_done);
+		}
+		int machine_id = jobs[j].gongxu_set[d].jiqi_id;
+		int process_time = jobs[j].gongxu_set[d].jiagong_time;
+		int t_arrive = trans_end[j][d];
+		int t_ready = (d == 0) ? 0 : op_end[j][d - 1];
+		int t_start = std::max({ t_arrive, t_ready, machine_free[machine_id] });
+		int t_finish = t_start + process_time;
+		op_end[j][d] = t_finish;
+		machine_free[machine_id] = t_finish;
+		op_done[j]++;
+	}
+	for (int i = 0; i < num_jobs; ++i)
+	{
+		int n_ops_job = (int)jobs[i].gongxu_set.size();
+		if (trans_done[i] <= n_ops_job)
+		{
+			this->schedule_transport(i + 1, n_ops_job, jobs, yt, vehicle_free, vehicle_pos, trans_done);
+		}
+	}
+	int makespan = 0;
+	for (int i = 0; i < num_jobs; ++i)
+	{
+		int n_ops_job = (int)jobs[i].gongxu_set.size();
+		makespan = std::max(makespan,trans_end[i][n_ops_job]);
+	}
+	fitness = makespan;
 }
--- JSPT/Chromosome.h
+++ JSPT/Chromosome.h
@@ -1,8 +1,14 @@
 #pragma once
 #include <vector>
-
+#include "yunshutime.h"
+#include "GONGJIAN.h"
 class Chromosome {
 public:
+
+	Chromosome(){}
+	~Chromosome() {}
+	Chromosome(const Chromosome& other);
+	Chromosome& operator=(const Chromosome& other);
     int fitness = 0;
     // 第一层：资源分配 [工件ID][第几次运输] -> 车辆ID
     std::vector<std::vector<int>> vehicle_assignment; 
@@ -12,6 +18,16 @@ public:
 
     // 第三层：运输顺序 (TS)
     std::vector<int> transport_sequence;
-    void calculate() {}
+    std::vector<std::vector<int>>op_end;
+    std::vector<std::vector<int>>trans_end;
+    void schedule_transport(
+        int job_id, int d,
+        const std::vector<GONGJIAN>& jobs,
+        const yunshutime& yt,
+        std::vector<int>& vehicle_free,
+        std::vector<int>& vehicle_pos,
+        std::vector<int>& trans_done
+    );
+    void calculate(const yunshutime& yt, const std::vector<GONGJIAN>& jobs) {}
     
 };
